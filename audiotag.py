4d5294f343ec5dd5bb1024d84a04b4c8ac4dc412
Rowan Greene <rowangreenedev@gmail.com>
2021-06-02 02:54:01 -0400

    Implement support for imports based on filetype

--- audiotag.py
+++ audiotag.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
 
 # Standard library imports
-import sys
+from sys import argv, stderr
 
 # Third-party imports
-# Import mutagen flac functionality, or fail gracefully.
 try:
-	from mutagen.flac import FLAC
+	import mutagen
 except ModuleNotFoundError:
-	print("Error loading Mutagen - ensure that Mutagen is installed and try again", file=sys.stderr)
+	print("Error loading Mutagen - ensure that Mutagen is installed and try again", file=stderr)
 	exit(1)
+
+if len(argv) < 2:
+	print(f"Usage: {argv[0]} FILE", file=stderr)
+	exit(1)
+
+supportedTypes = ["flac"]
+filetype = argv[1].split(".")[-1].casefold()
+
+if filetype == "flac":
+	from mutagen.flac import FLAC
+	song = FLAC(argv[1])
+else:
+	print("Error: unsupported filetype. Supported types are: {supportedTypes}", file=stderr)
+
 
 helpstrs = {"help": "help [COMMAND]: display help for COMMAND, or a list of " \
 		"accepted commands if none is given", "addTag": "addTag TAG "\
@@ -73,25 +86,9 @@ def deleteTag(*args):
 		return 0
 	
 	except ValueError:
-		print("Tag not found", file=sys.stderr)
+		print("Tag not found", file=stderr)
 		return 1
 	
-
-
-# Because there is no argc
-argc = len(sys.argv)
-
-# For consistency with argc
-argv = sys.argv
-
-if (argc < 2):
-	print(f"Usage: {argv[0]} FILE", file=sys.stderr)
-	exit(1)
-
-# If we aren't given a flac file, print an error message and exit
-if (argv[1].split(".")[-1].casefold() != "flac"):
-	print("Error: unsupported file type.", file=sys.stderr)
-	exit(1)
 
 # Load song
 song = FLAC(argv[1])
@@ -119,7 +116,7 @@ while (True):
 	# loop
 	if (cmd not in commands.keys()):
 		print(f"Error: {cmd}: invalid command. Run `help` for help.",
-		file=sys.stderr)
+		file=stderr)
 		continue
 	
 	# If we have a valid command, pass args to the command associated with
